cc98b58fe9966f079984d14e4f55c77ba2dfc7d2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-31 19:25:29 +0000

    Deploy goodwin64/free-parkings-ui to github.com/goodwin64/free-parkings-ui.git:gh-pages

---
+++ precache-manifest.704eb4ce6f098311cfcd385737883e2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bffebb89977a310460c5",
+    "url": "/free-parkings-ui/static/css/main.6fed29b3.chunk.css"
+  },
+  {
+    "revision": "bffebb89977a310460c5",
+    "url": "/free-parkings-ui/static/js/main.bffebb89.chunk.js"
+  },
+  {
+    "revision": "d591d16800c6608428e9",
+    "url": "/free-parkings-ui/static/css/1.e14070b3.chunk.css"
+  },
+  {
+    "revision": "d591d16800c6608428e9",
+    "url": "/free-parkings-ui/static/js/1.d591d168.chunk.js"
+  },
+  {
+    "revision": "e213c264ca9821c2bbdc",
+    "url": "/free-parkings-ui/static/js/runtime~main.e213c264.js"
+  },
+  {
+    "revision": "2931e6af560a445dc9ad9b73711b0f02",
+    "url": "/free-parkings-ui/static/media/magnifier.2931e6af.svg"
+  },
+  {
+    "revision": "d3fdc5a19c2817bfbd2b8a1949fa0f30",
+    "url": "/free-parkings-ui/index.html"
+  }
+];
