57fecb19bf11a259fe928ed7a2d60c497bf3f869
Max Donchenko <Maksym.Donchenko@cloudmade.com>
2019-05-07 11:53:22 +0300

    fix test

--- src/store/parkings/adapters.test.ts
+++ src/store/parkings/adapters.test.ts
@@ -2,9 +2,6 @@ import { prepareParkings } from './adapters';
 
 describe('prepareParkings adapter', () => {
   it('should create default Parkings object if falsy response passed', () => {
-    expect(prepareParkings()).toEqual({
-      allParkings: [],
-      freeParkings: [],
-    });
+    expect(prepareParkings()).toEqual([]);
   });
 });
